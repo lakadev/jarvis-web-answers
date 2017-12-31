e4390ea5dc0745b5aa936faba2064da2515aca86
Djam <gc.djames@gmail.com>
2017-12-31 15:31:52 +0100

    Correction of installation for Raspberry Pi

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # This file will be automatically sourced at the installation of your plugin
 # Use only if you need to perform changes on the user system such as installing software
 jv_debug "Downloading pup parser"
-jv_pg_wa_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+jv_pg_wa_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/
 if [[ "$platform" == "linux" ]]; then
 	# Start in new terminal to avoid blocking Jarvis              
 	wget "https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_arm.zip"
