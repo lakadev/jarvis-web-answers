b6a035224a634f8198d8349dd9ec6a1ad594f9e5
Djam <gc.djames@gmail.com>
2017-12-31 15:45:04 +0100

    Correction of installation for Raspberry Pi

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ jv_debug "Downloading pup parser"
 jv_pg_wa_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/
 if [[ "$platform" == "linux" ]]; then
 	# Start in new terminal to avoid blocking Jarvis              
-	wget "https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_arm.zip"
-	unzip -j "pup_v0.4.0_linux_arm.zip" "websocketd" -d $jv_pg_wa_root
-	rm pup_v0.4.0_linux_arm.zip
-elif [[ "$platform" == "osx" ]]; then
-    wget "https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_arm64.zip"
-    unzip -j "pup_v0.4.0_linux_arm64.zip" "pup" -d $jv_pg_wa_root
+	wget "https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_arm64.zip"
+	unzip -j "pup_v0.4.0_linux_arm64.zip" "pup" -d $jv_pg_wa_root
 	rm pup_v0.4.0_linux_arm64.zip
+elif [[ "$platform" == "osx" ]]; then
+    wget "https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_darwin_amd64.zip"
+    unzip -j "pup_v0.4.0_darwin_amd64.zip" "pup" -d $jv_pg_wa_root
+	rm pup_v0.4.0_darwin_amd64.zip
 fi
